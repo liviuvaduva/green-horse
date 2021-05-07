e22c1b413b6a4a9de2c5faafc4402d12304fc731
liviu.vaduva <liviu.vaduva@yahoo.com>
2021-05-07 12:24:54 +0300

    [13] Crate base.blade.php

---
+++ laravel/resources/views/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GreenHorse @yield('title')</title>
+
+    <script type="text/javascript" src="{{ mix('/js/app.js')  }}"></script>
+
+    @section('javascript')
+        <!-- Page's specific javascript -->
+    @endsection
+</head>
+<body>
+<p>@todo: implement view</p>
+</body>
+</html>
